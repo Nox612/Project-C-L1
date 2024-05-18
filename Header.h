7190a64d0929972cb0a06f8739595bcd7acced38
Hukaiy <151056790+Hukaiy@users.noreply.github.com>
2024-05-19 00:06:32 +0200

    Update Header.h

--- Header.h
+++ Header.h
@@ -29,3 +29,36 @@ typedef struct{
 
 CDATAFRAME *create_cdf();
 int fill_cdf(CDATAFRAME *cdataframe, COLUMN *column);
+// prj part 2
+
+//  5.1
+enum enum_type
+{
+NULLVAL = 1 , UINT, INT, CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
+};
+typedef enum enum_type ENUM_TYPE;
+
+union column_type{
+unsigned int uint_value;
+signed int int_value;
+char char_value;
+float float_value;
+double double_value;
+char* string_value;
+void* struct_value;
+};
+typedef union column_type COL_TYPE ;
+
+struct column {
+char *title;
+unsigned int size; //logical size
+unsigned int max_size; //physical size
+ENUM_TYPE column_type;
+COL_TYPE **data; // array of pointers to stored data
+unsigned long long int *index; // array of integers
+};
+typedef struct column COLUMN;
+
+//5.1.1
+COLUMN *create_column(ENUM_TYPE type, char *title);
+int insert_value(COLUMN *col, void *value);
