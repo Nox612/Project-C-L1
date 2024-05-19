0167622d7ff3e7ac1d2ff9c5d1bbdb178f96aade
Hukaiy <151056790+Hukaiy@users.noreply.github.com>
2024-05-19 13:01:05 +0200

    Update Function.c

--- Function.c
+++ Function.c
@@ -91,25 +91,75 @@ int equal2(COLUMN *col, int value){
     }
     return res;
 }
-COLUMN *create_column(ENUM_TYPE type, char *title)
+
+COLUMN *create_column2(ENUM_TYPE type, char *title)
 {
     COLUMN *col = (COLUMN*)malloc(sizeof(COLUMN));
     col -> title = title;
     col -> size = 0;
     col -> max_size = 0;
     col -> column_type; //jsp cmt faire pr cette ligne
-    col -> data = 0;
+    col -> *data = 0;
     col -> index = 0;
 
-    return colonne;
+    return col;
 }
 
-int insert_value(COLUMN *col, void *value)
+int insert_value2(COLUMN *col, void *value)
 {
-    if (col -> size == col -> max_size)
+    if (col -> lSize == col -> pSize)
     {
         col->data = malloc(REALOC_SIZE*sizeof(int));
-        col->size += REALOC_SIZE;
-         
+        col->pSize += REALOC_SIZE;
+    }
+    
+    if (value == NULL)
+    {
+        *value = NULL;
+        col -> data[col->pSize] = *value;
+    }
+    if (col->data!=NULL)
+        {
+            col->data[col->pSize] = *value;
+            col->pSize += 1;
+            return 1;
+        }
+
+    return 0;
+}
+void delete_column2(COLUMN **col)
+{
+    free(col->data);
+    free(col);
+}
+
+void convert_value(COLUMN *col, unsigned long long int i, char *str, int size)
+{
+    switch(col->column_type)
+    {
+        case INT :
+        snprintf(str, size, "%d", *((int*)col->data[i]));
+        break;
+
+        case CHAR :
+        snprintf(str, size, " %c", *((char*)col->data[i]));
+        break;
+
+        case FLOAT :
+        snprintf(str, size, "%f", *((float*)col->data[i]));
+        break;
+
+        case DOUBLE:
+        snprintf(str, size, "%ld", *((double*)col->data[i]));
+        break;
+
+        case STRING:
+        snprintf(str, size, "%s", ((char)col->data[i]));
+        break;
+
+        case STRUCTURE:
+        snprintf(str, size, "%s", ((void*)col->data[i]));
+        break;
     }
 }
+
